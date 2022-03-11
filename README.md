61d81750b9bf6afe6e13daec9de3700765df7bee
jkingsman <jack.kingsman@gmail.com>
2022-03-10 22:55:18 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Link2Wiki
+
+## Description
+
+This Chrome extension injects a link to Wikipedia search (which will, for most pages, result in a redirect to the main page) for all search terms in a Google query.
+
+Link Logo by Hassan ali from NounProject.com, CC-BY
---
+++ src/injector.js
@@ -0,0 +1,51 @@
+/**
+ * Add a link to Wikipedia search above Google search results.
+ *
+ * This project is licensed under the terms of the MIT license.
+ *
+ * @author      Jack Kingsman <jack.kingsman@gmail.com>
+ *
+ */
+"use strict";
+
+function elementReady(selector) {
+  // Function author: jwilson8767; https://gist.github.com/jwilson8767/db379026efcbd932f64382db4b02853e
+  // MIT Licensed
+
+  return new Promise((resolve, reject) => {
+    let el = document.querySelector(selector);
+    if (el) {
+      resolve(el);
+      return;
+    }
+
+    new MutationObserver((mutationRecords, observer) => {
+        // Query for elements matching the specified selector
+        Array.from(document.querySelectorAll(selector)).forEach((element) => {
+          resolve(element);
+          //Once we have resolved we don't need the observer anymore.
+          observer.disconnect();
+        });
+      })
+      .observe(document.documentElement, {
+        childList: true,
+        subtree: true
+      });
+  });
+}
+
+function parseAndInjectAtTopOfElem(elem) {
+  const query = (new URLSearchParams(window.location.search.slice(1))).get("q");
+  if (query) {
+    const a = document.createElement('a');
+    a.href = `https://en.wikipedia.org/w/index.php?search=${query}`
+    a.innerHTML = '🔗 Wiki Link'
+    a.target = '_blank'
+    a.rel = 'noreferrer noopener'
+    a.referrerpolicy = 'no-referrer'
+
+    elem.prepend(a);
+  }
+}
+
+elementReady('#search').then((elem)=>{parseAndInjectAtTopOfElem(elem)});
